5bcb14b481136fa6211e70b797a5e6ff68f34e03
Aluno talentos <raul.hiago.salomao@gmail.com>
2023-04-26 17:08:55 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,102 @@
+//Exercicios de escrita de codigo
+
+//const Idade = prompt ("Qual sua idade?")
+//const Idade2 = prompt ("Qual idade do seu melhor amigo?")
+//console.log ("Sua idade:",Idade)
+//console.log ("Idade do seu melhor amigo",Idade2)
+//console.log ("Sua idade e maior do que a do seu melhor amigo?",Idade>Idade2)
+//console.log ("diferença de idade", Idade - Idade2)
+
+//const numeropar = prompt ("Insira um numero par")
+//const numerodivisao = numeropar % 2
+//console.log (numerodivisao)
+   //um numero par divido por um numero par da 0
+   //um numero par divido par um numero impar da 1
+   //se o usuaio escrever um numero impar o resto sera 1
+
+  //const idadeano = prompt ("Digite sua idade")
+  //const idademeses = idadeano *12
+  //const idadedias = idadeano * 365
+  //const idadehoras = idadedias * 24
+  //console.log ("Sua idade em meses:",idademeses)
+  //console.log ("sua idade em dias:",idadedias)
+  //console.log ("sua idade em horas:",idadehoras)
+
+//const Numero1 = prompt ("Digite um numero")
+//const Numero2 = prompt ("digite outro numero")
+//const numero3 = Numero1 % Numero2
+//const numero4 = Numero2 % Numero1
+//console.log ("Primeiro numero e maior do que o segundo?", Numero1 < Numero2)
+//console.log ("O primeiro numero e igual ao segundo?",Numero1===Numero2)
+//console.log  ("O primeiro numero e divisivel pelo segundo?",numero3===0)
+//console.log ("O segundo numero e divisivel pelo primeiro?", numero4===0)
+
+//Desafio
+
+//const fahrenheit 77
+//const kelvin  = (fahrenheit-32)*(5/9)+273.15
+//console.log (kelvin)
+
+//const celsius = 80
+//const fahrenheit = (celsius)*(9/5)+32
+//console.log(fahrenheit)
+
+//const celsius = 30
+//const fahrenheit = (celsius)*(9/5)+32
+//const kelvin = (fahrenheit-32)*(5/9)+273.15
+//console.log ("Valor em fahrenheit",fahrenheit)
+//console.log ("valor em kelvin",kelvin)
+
+//const celsius = prompt ("Insira a temperatura em graus")
+//const fahrenheit = (celsius)*(9/5)+32
+//const kelvin = (fahrenheit-32)*(5/9)+273.15
+//console.log ("Valor em fahrenheit",fahrenheit)
+//console.log ("valor em kelvin",kelvin)
+
+//const quilowatt = Number (prompt ("Insira o valor"))
+//const desconto = Number (prompt ("Insira o valor do desconto"))
+//const quilowattvalor = quilowatt*0.05
+//const quilowattdesc = quilowattvalor * (desconto/100)
+//const qlwd = quilowattvalor - quilowattdesc 
+//console.log ("Valor gasto por hora:",quilowattvalor)
+//console.log ("Valor gasto por hora com desconto:",qlwd)
+
+//const libra = 20
+//const kg = 0.4536 
+//const kgconv = libra*kg
+//console.log ("Valor em kg:",kgconv)
+
+//const oz = 10.5
+//const kg = 0.0283 
+//const kgconv = oz*kg
+//console.log ("Valor em kg:",kgconv
+
+//const mi = 100
+//const m = 0.00062137
+//const mconv = mi/m
+//console.log ("Valor em metros:",mconv)
+
+//const ft = 50
+//const metro = 0.3048 
+//const metroconv= ft*metro
+//console.log ("Valor em metros",metroconv)
+
+//const gal = 103.56
+//const litros = 3.7854
+//const lconv = gal*litros
+//console.log ("Valor em litros",lconv)
+
+//const xic=450
+//const litro=0.24
+//const lconv = xic*litro
+//console.log ("Valor em litros:",lconv)
+
+//const mi = prompt ("insira o valor de milhas")
+//const m = 0.00062137
+//const mconv = mi/m
+//console.log ("Valor em metros:",mconv)
+
+//const ft = prompt ("Insira o valor em pés")
+//const metro = 0.3048 
+//const metroconv= ft*metro
+//console.log ("Valor em metros",metroconv)
